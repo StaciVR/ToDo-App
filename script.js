9c5c461a71185d6f49f8230f147b27bb5724fe64
StaciVR <stacivanrooy@gmail.com>
2022-06-25 18:30:10 +0200

    Update script.js
    added edit event and save

--- script.js
+++ script.js
@@ -32,6 +32,13 @@ function addNew() {
 
     document.querySelector(".itemListBox").appendChild(editBtn);
 
+    const saveBtn = document.createElement("button");
+    saveBtn.className = "editBtnStyle";
+    saveBtn.innerHTML = "save";
+
+    document.querySelector(".itemListBox").appendChild(saveBtn);
+
+
     //create element for delete button
     const removeBtn = document.createElement("button");
 
@@ -62,6 +69,21 @@ function addNew() {
     })
 
 
+    // add event listener to make input editable when edit button is clicked
+
+    editBtn.addEventListener("click", () => {
+        newItem.contentEditable = true;
+        newItem.style.backgroundColor = "rgba(250, 250, 250, 0.2)";
+
+    })
+
+    // add event listener to make input not editable when save button is clicked
+
+    saveBtn.addEventListener("click", () => {
+        newItem.contentEditable = false;
+        newItem.style.backgroundColor = "rgba(250, 250, 250, 0.8)";
+
+    })
 }
 
 
