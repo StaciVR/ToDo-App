58b17af468552e6c53d6518fb6b9f6b3babeeb08
StaciVR <stacivanrooy@gmail.com>
2022-06-25 16:00:58 +0200

    added buttons
    added delete, edit & complete button via js and added styling in css for those buttons

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const input = document.getElementById("userInput");
 const taskList = document.getElementById("itemList");
 
+
 document.getElementById("submitButton").addEventListener("click", () => {
+
     if (input.value == "") {
         alert("Enter new task");
     } else {
         addNew();
     }
-
     input.value = "";
-
 })
+
+// to create new task item
 
 function addNew() {
     const newItem = document.createElement("div");
@@ -19,4 +21,29 @@ function addNew() {
 
     document.querySelector(".itemListBox").appendChild(newItem);
 
+    const editBtn = document.createElement("button");
+
+    editBtn.className = "editBtnStyle";
+    editBtn.innerHTML = "edit";
+
+    document.querySelector(".itemListBox").appendChild(editBtn);
+
+    const removeBtn = document.createElement("button");
+
+    removeBtn.className = "buttonStyle";
+    removeBtn.innerHTML = "delete";
+
+    document.querySelector(".itemListBox").appendChild(removeBtn);
+
+    const completeBtn = document.createElement("button");
+
+    completeBtn.className = "completeBtnStyle";
+    completeBtn.innerHTML = "complete";
+
+    document.querySelector(".itemListBox").appendChild(completeBtn);
+
+
 }
+
+
+
