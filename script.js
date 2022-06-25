ee1681920f443b0b283952782407d8aab7da6127
StaciVR <stacivanrooy@gmail.com>
2022-06-25 02:49:43 +0200

    updates js
    added javascript for list items

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const input = document.getElementById("userInput");
 const taskList = document.getElementById("itemList");
 
 document.getElementById("submitButton").addEventListener("click", () => {
-    document.getElementById("itemList").innerHTML = input.value;
+    const newItem = document.createElement("div");
+    newItem.className = "itemList";
+    newItem.innerHTML = input.value;
+
+    document.querySelector(".itemListBox").appendChild(newItem);
+
+    input.value = "";
+
 })
+
