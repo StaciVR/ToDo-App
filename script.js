b5bca26d9ce05daf9f11c396ca4484cf05b5aad9
StaciVR <stacivanrooy@gmail.com>
2022-06-25 03:01:20 +0200

    image update

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const input = document.getElementById("userInput");
 const taskList = document.getElementById("itemList");
 
 document.getElementById("submitButton").addEventListener("click", () => {
-    const newItem = document.createElement("div");
-    newItem.className = "itemList";
-    newItem.innerHTML = input.value;
-
-    document.querySelector(".itemListBox").appendChild(newItem);
+    if (input.value == "") {
+        alert("Enter new task");
+    } else {
+        addNew();
+    }
 
     input.value = "";
 
 })
 
+function addNew() {
+    const newItem = document.createElement("div");
+    newItem.className = "itemList";
+    newItem.innerHTML = input.value;
+
+    document.querySelector(".itemListBox").appendChild(newItem);
+}
